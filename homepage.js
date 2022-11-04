a985beb60cd694045381bbd6e29780033388528e
mustafadede <mustafa.dede.0016@gmail.com>
2022-11-04 20:59:29 +0300

    filtering and font localstorage

--- homepage.js
+++ homepage.js
@@ -5,11 +5,15 @@ const textArea = document.querySelector("#text-area");
 const add = document.querySelector("#ekle");
 const spaceField = document.querySelector(".space");
 const localStorageValue = localStorage.getItem("username");
-
+const bodyFieldArea = document.body;
+const logout = document.querySelector(".logout");
+console.log(logout);
 username.value = localStorageValue;
 let data = localStorage.getItem("todos");
 let textAreaInfo = data ? JSON.parse(data) : [];
-console.log(textAreaInfo);
+
+let fontType = localStorage.getItem("selectedFont");
+let fontInfo = fontType ? JSON.parse(fontType) : [];
 
 // local storage deletion improvement!!!
 function localStorageDeletion(pressedValue) {
@@ -30,6 +34,7 @@ function addTodos() {
   p.textContent = textArea.value;
   deleteBtn.textContent = "Sil";
   deleteBtn.classList.add("deletebtn");
+  deleteBtn.style.fontFamily = fontInfo;
   wrapper.appendChild(checkBox);
   wrapper.appendChild(p);
   wrapper.appendChild(deleteBtn);
@@ -55,6 +60,7 @@ function addtodoslocalstorage(data) {
     p.textContent = value;
     deleteBtn.textContent = "Sil";
     deleteBtn.classList.add("deletebtn");
+    deleteBtn.style.fontFamily = fontInfo;
     wrapper.appendChild(checkBox);
     wrapper.appendChild(p);
     wrapper.appendChild(deleteBtn);
@@ -82,3 +88,9 @@ add.addEventListener("click", function () {
 if (textAreaInfo.length > 0) {
   addtodoslocalstorage(textAreaInfo);
 }
+
+bodyFieldArea.style.fontFamily = fontInfo;
+add.style.fontFamily = fontInfo;
+textArea.style.fontFamily = fontInfo;
+username.style.fontFamily = fontInfo;
+logout.style.fontFamily = fontInfo;
--- modal.js
+++ modal.js
@@ -4,44 +4,41 @@ const modalContainer = document.querySelector(".modal-container");
 const backdrop = document.querySelector(".backdrop");
 const closeBtn = document.querySelector(".close-button");
 const deleteUser = document.querySelector(".deleteuser");
-const fontInput = document.querySelector(".font-input");
 const pFonts = document.querySelectorAll(".font-names");
 const themeColor = document.querySelectorAll(".theme-color");
 const bodyField = document.body;
+const fontCard = document.querySelectorAll(".font-card");
 const fontNames = ["Kanit", "Merriweather", "Roboto", "Shalimar"];
 // font card text changes
 pFonts.forEach((font, index) => {
   font.style.fontFamily = fontNames[index];
 });
 
-// function themeChange(selected, themeInfo) {
-//   if (themeInfo === "dark") {
-//     selected.classList.add("dark-theme");
-//   } else if (themeInfo === "very-dark") {
-//     selected.classList.add("high-contrast");
-//   } else {
-//     selected.classList.contains(themeInfo)
-//       ? selected.classList.remove("dark-theme")
-//       : selected.classList.remove("very-dark");
-//   }
-// }
+fontCard.forEach((card) => {
+  card.addEventListener("click", function () {
+    localStorage.setItem("selectedFont", JSON.stringify(card.innerText));
+    bodyField.style.fontFamily = card.innerText;
+    location.reload();
+  });
+});
 
-// themeColor.forEach((selected) => {
-//   console.log(selected.classList[1]);
-//   selected.addEventListener(
-//     "click",
-//     themeChange(bodyField, selected.classList[1])
-//   );
-// });
-
-function inputFilter(e) {
-  console.log(e);
+function inputHandler() {
+  const fontInputValues = document.querySelector(".font-input").value;
+  fontCard.forEach((card) => {
+    // console.log(value.innerText);
+    if (card.innerText.toLowerCase().includes(fontInputValues)) {
+      card.style.display = "flex";
+    } else {
+      card.style.display = "none";
+    }
+  });
 }
 
-fontInput.addEventListener("keypress", inputFilter);
 // deleting localstorage values
 deleteUser.addEventListener("click", function () {
-  localStorage.removeItem("todos") & localStorage.removeItem("username");
+  localStorage.removeItem("todos") &
+    localStorage.removeItem("username") &
+    localStorage.removeItem("selectedFont");
   location.href = "index.html";
 });
 
